a58531d8975b8e11bb45cfd7f0d8aeec9fce80dd
Ekene Chris-Oparah <ekene.chrisoparah@kuda.com>
2023-02-28 00:13:38 +0100

    Last one

--- terraform.tf
+++ terraform.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "=3.0.0"
     }
   }
-  provider "azurerm" {
-  features {}
 }
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
